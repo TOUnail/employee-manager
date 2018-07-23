5457e2914fcde4078dee092eabf64d23f425092b
alextou <a.tou287@gmail.com>
2018-07-22 21:04:03 -0700

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBeZFi1NyCin72A3akyuybTskHMbGSxZbo",
+    authDomain: "vuefs-prod-a2179.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-a2179.firebaseio.com",
+    projectId: "vuefs-prod-a2179",
+    storageBucket: "vuefs-prod-a2179.appspot.com",
+    messagingSenderId: "402848752015"
+}
